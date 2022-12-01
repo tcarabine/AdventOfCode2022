84c2e1d681ecb4cd25df82cef3374541194ef58c
Tom Carabine <12697250+tcarabine@users.noreply.github.com>
2022-12-01 23:03:29 +0000

    day1

---
+++ src/day01/Program.fs
@@ -0,0 +1,46 @@
+﻿namespace Day1
+
+open System.IO
+open System.Linq
+
+module Functions = 
+  
+  let bundle (raw: string) : List<string> = raw.Split "\n\n" |> Array.toList
+
+  let injest (filePath: string): string = File.ReadLines(filePath) |> Seq.reduce (fun (a: string) (b: string) -> $"{a}\n{b}") 
+
+  let sum (raw: string) : int = 
+    raw.Split "\n"
+      |> Array.toList
+      |> List.map (fun str -> int(str))
+      |> List.sum
+
+  let most (input: string): int = 
+    input|> bundle
+      |> List.map (fun grp -> sum grp )
+      |> List.max
+
+  let top3 (input: string): int =
+    let arr = 
+      input
+      |> bundle
+      |> List.map (fun grp -> sum grp )
+      |> List.sortDescending
+      |> List.toArray
+    arr[0..2]
+    |> Array.sum
+module Program = 
+  [<EntryPoint>] 
+  let main args = 
+    let maxCals = 
+      Functions.injest "input.txt"
+      |> Functions.most
+
+    printfn $"{maxCals}"
+
+    let top3 = 
+      Functions.injest "input.txt"
+      |> Functions.top3
+    
+    printfn $"{top3}"
+    0
---
+++ tests/day01.tests/Tests.fs
@@ -0,0 +1,39 @@
+module Tests
+
+open System
+open Xunit
+open FluentAssertions
+
+open Day1
+
+[<Fact>]
+let ``Functions.bundle should produce 2 items when given a string with a single \n\n`` () =
+  //Arrange
+  let input = "line1\nline2\n\nline4"
+
+  //Act
+  let result = Functions.bundle input
+
+  //Assert
+  result.Should().HaveCount(2, "There is only one \\n\\n provided")
+
+[<Fact>]
+let `` Functions.most should return 24000 for the sample input`` () =
+  //Arrange 
+  let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+
+  //Act
+  let result : int= Functions.most input
+
+  //Assert
+  result.Should().Be(24000, "this is the sample value")
+[<Fact>]
+let ``Functions.top3 should return 45000 for the sample input``   () =
+  //Arrange 
+  let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+
+  //Act
+  let result : int= Functions.top3 input
+
+  //Assert
+  result.Should().Be(45000, "this is the sample value")
